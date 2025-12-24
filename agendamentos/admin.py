d345685e59a0a00d8a75bd479354cc2edce0f1b7
Octavio7S <octaviosoaresdarosa@gmail.com>
2025-12-24 12:17:56 -0300

    Criando os primeiros models

--- agendamentos/admin.py
+++ agendamentos/admin.py
@@ -1,3 +1,15 @@
 from django.contrib import admin
+from .models import Cliente, Servico, Agendamento
 
 # Register your models here.
+@admin.register(Cliente)
+class AdminCliente(admin.ModelAdmin):
+    ...
+
+@admin.register(Servico)
+class AdminServico(admin.ModelAdmin):
+    ...
+
+@admin.register(Agendamento)
+class AdminAgendamento(admin.ModelAdmin):
+    ...
--- agendamentos/models.py
+++ agendamentos/models.py
@@ -1,3 +1,28 @@
 from django.db import models
 
 # Create your models here.
+
+class Cliente(models.Model):
+    nome = models.CharField(max_length=80)
+    telefone = models.CharField(max_length=25)
+
+    def __str__(self):
+        return self.nome
+    
+
+class Servico(models.Model):
+    nome_servico = models.CharField(max_length=20)
+    duracao = models.IntegerField()
+
+    def __str__(self):
+        return self.nome_servico
+
+
+class Agendamento(models.Model):
+    cliente = models.ForeignKey(Cliente, on_delete=models.CASCADE)
+    servico = models.ForeignKey(Servico, on_delete=models.CASCADE)
+    data = models.DateField()
+    hora_inicio = models.TimeField()
+
+#    def __str__(self):
+#        return self.servico
